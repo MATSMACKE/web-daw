cedf9ca388b306b68055f3680f818997da6d999b
MATSMACKE <61540264+MATSMACKE@users.noreply.github.com>
2021-12-05 16:54:41 +0100

    continuous playback with RBJ EQ

---
+++ src/components/ChannelRack.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ChannelRack"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainBody.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MainBody"
+}
+</script>
+
+<style scoped>
+
+</style>
